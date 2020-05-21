2096ca1af9f740c0f0324c55a2a43c389812f44c
Thongchai <thongchai.c@srru.ac.th>
2020-05-21 17:00:30 +0700

    update api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // api_url: 'https://surin.srru.ac.th/api',
   api_url: '/api',
-  gmap_api_key: 'AIzaSyAqiHOgtttCiHMRunb67vOkcSoa5y9kQ14'
+  gmap_api_key: 'AIzaSyAqXovEezhbGZJDB-fKok9QDax0iSr3iKQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   api_url: 'https://surin.srru.ac.th/api',
   // api_url: '/api',
-  gmap_api_key: 'AIzaSyAqiHOgtttCiHMRunb67vOkcSoa5y9kQ14'
+  gmap_api_key: 'AIzaSyAqXovEezhbGZJDB-fKok9QDax0iSr3iKQ'
 };
 
 /*
